ee7c73af2f8f90969fb910c4ac367448acd45291
Patrick Hawkins <phawkins@liquidweb.com>
2013-12-19 15:19:03 -0500

    /all gives you ascii 1-255

--- mungo.rb
+++ mungo.rb
@@ -4,7 +4,15 @@ get '/' do
   'This should eventually be a usage page'
 end
 
+get '/all' do
+  all = (1..255).to_a
+  all.map! { |x| x.chr }
+  all = all.join
+  "#{all}"
+end
+
 get '/:num/*' do |num, word|
   output = word * num.to_i
   "#{output}"
 end
+
