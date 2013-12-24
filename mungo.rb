8515d2d35511182368685f2c57ab4dec58d2ce00
Patrick Hawkins <phawkins@liquidweb.com>
2013-12-24 16:07:04 -0500

    counterstring feature

--- mungo.rb
+++ mungo.rb
@@ -12,6 +12,34 @@ get '/all' do
   "#{all}"
 end
 
+get '/count/*' do |target|
+  @target = target.to_i
+  @pos = @target
+  @text = ""
+  @pip = "*"
+
+  while true do
+    if @text.length + @pos.to_s.length + 1 > @target
+      if @target - @text.length == 0
+        @text.reverse!
+        break
+      else
+        @text.insert(-1, @pip)
+        @text.reverse!
+        break
+      end
+    else
+      @text.insert(-1, @pip)
+      insertion = @pos.to_s
+      insertion.reverse!
+      @text.insert(-1, insertion)
+      @pos = @pos - @pos.to_s.length-1
+    end
+  end
+  "#{@text}"
+  
+end
+
 get '/:num/*' do |num, word|
   output = word * num.to_i
   "#{output}"
